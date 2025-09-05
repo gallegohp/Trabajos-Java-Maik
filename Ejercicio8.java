b4b3b4b3df4f9d31b17b6b86b1cece352b0fdd56
Camilo Gallego <c.gallego3029@gmail.com>
2025-09-05 08:43:42 -0500

    CG - Commit Trabajos completos 1.9

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 public class Ejercicio8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("¿Cuántos números de Fibonacci quieres ver? ");
+        System.out.print("Cuantos números de Fibonacci quieres ver? ");
         int n = scanner.nextInt();
-        int a = 0, b = 1;
+        int a = 0;
+        int b = 1;
 
         System.out.println("Serie Fibonacci:");
         for (int i = 0; i < n; i++) {
